2e3e7a85e7968636376c3fb3ea93a7af4a51a9ee
Andie <andyespino07@hotmail.com>
2025-09-03 04:30:52 -0600

    Añadí mi parte, texto y poster

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        sand: "#F0EDE5",
+        jet: "312F2C",
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"],
+        Ubuntu: ["Ubuntu", "sans-serif"],
+      },
+    },
+  },
+}
